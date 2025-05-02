49db25da4c154ae6ab493b9035f4c398cb553165
Nazmul Hossen <89706757+Nazmul580@users.noreply.github.com>
2025-05-03 02:15:19 +0600

    delete unwanted login and register api and modify auth rout to implement generate access token api intregration

--- blog-backend/routes/rootRouter.js
+++ blog-backend/routes/rootRouter.js
@@ -4,9 +4,6 @@ const postRoutes = require("./posts");
 const commentRoutes = require("./comments");
 const analyticRoutes = require("./analytics");
 const adminRoutes = require("./admin");
-const registerRouter = require("./register");
-const loginRouter = require("./login");
-const acessTokenRouter = require("./acessToken");
 const rootRouter = (app) => {
   app.use("/api/auth", authRoutes);
   app.use("/api/users", userRoutes);
@@ -14,9 +11,6 @@ const rootRouter = (app) => {
   app.use("/api/comments", commentRoutes);
   app.use("/api/analytics", analyticRoutes);
   app.use("/api/admin", adminRoutes);
-  app.use("/api/register", registerRouter);
-  app.use("/api/login", loginRouter);
-  app.use("/api/acessToken", acessTokenRouter);
 };
 
 module.exports = rootRouter;
