511a47e0cbe64047fa6de4e5648c0e93abe1c013
looser <sabbirahmad653@gmail.com>
2025-05-05 14:35:38 +0600

    making new brnach

--- blog-backend/models/Post.js
+++ blog-backend/models/Post.js
@@ -10,7 +10,7 @@ const postSchema = new mongoose.Schema({
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   category: { type: String, required: true },
-  image: { type: String }, // 
+  image: { type: String }, 
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   views: { type: Number, default: 0 },
   polls: [pollSchema],
